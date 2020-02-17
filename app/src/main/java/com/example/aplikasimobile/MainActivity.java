ab2e825946ad141efe248cde37139c2915c2ba6f
nadiapd <nadia.putri.tif18@polban.ac.id>
2020-02-17 16:41:51 +0700

    first commit

---
+++ app/src/main/java/com/example/aplikasimobile/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.aplikasimobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    //Deklarasi Variabel
+    EditText TextNama;
+    TextView Hasil;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Panggil Variabel Berdasarkan id
+        TextNama=(EditText)findViewById(R.id.TxtNama);
+        Hasil=(TextView)findViewById(R.id.Lbl_Nama);
+    }
+
+    public void TampilNama(View v) {
+        Hasil.setText("Nama Anda: "+TextNama.getText());
+    }
+}
